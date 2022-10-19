8de4ee1490419114bc4930ecf23d50d46b8cc274
Drozd_AS <31595916+SD-SanaZI@users.noreply.github.com>
2022-10-19 14:36:24 +0300

    Initial commit

---
+++ app/src/main/java/com/example/testbitcoin/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.testbitcoin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val textView: TextView = findViewById(R.id.title)
+        textView.setOnClickListener{OpenSecondActivity()}
+    }
+
+    private fun OpenSecondActivity(){}
+}
